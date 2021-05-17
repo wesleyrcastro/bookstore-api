02cf4db8fe9cde4a4aa078fabc87c8e6fa8500ef
wesleyrcastro <wesleyrcastro@yahoo.com.br>
2021-05-16 21:34:24 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/wesley/bookstore/domain/Livro.java
@@ -0,0 +1,78 @@
+package com.wesley.bookstore.domain;
+
+import java.util.Objects;
+
+public class Livro {
+
+    private Integer id;
+    private String titulo;
+    private String nomeAutor;
+    private String texto;
+
+    private Categoria categoria;
+
+    public Livro() {
+        super();
+    }
+
+    public Livro(Integer id, String titulo, String nomeAutor, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nomeAutor = nomeAutor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return id.equals(livro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
