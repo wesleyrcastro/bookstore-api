f61bb075dfef96419b3973d37553bafec0e3719a
wesleyrcastro <wesleyrcastro@yahoo.com.br>
2021-05-22 15:41:42 -0300

    findAll para Categoria utilizando padrão DTO

--- src/main/java/com/wesley/bookstore/service/CategoriaService.java
+++ src/main/java/com/wesley/bookstore/service/CategoriaService.java
@@ -6,6 +6,7 @@ import com.wesley.bookstore.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +19,9 @@ public class CategoriaService {
         Optional<Categoria> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " +id+ ", Tipo: " + Categoria.class.getName()));
+    }
+
+    public List<Categoria> findAll() {
+        return repository.findAll();
     }
 }
