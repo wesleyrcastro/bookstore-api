36d71c5ae369f65359995f4e447e3bd3c4bb50ec
wesleyrcastro <wesleyrcastro@yahoo.com.br>
2021-05-22 22:15:46 -0300

    Listando todos os livros por categorias

--- src/main/java/com/wesley/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/wesley/bookstore/repositories/LivroRepository.java
@@ -2,9 +2,15 @@ package com.wesley.bookstore.repositories;
 
 import com.wesley.bookstore.domain.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Integer> {
 
+    @Query("select obj from livro obj where obj.categoria.id = :id_cat order by obj.titulo")
+    List<Livro> findAllCategoria(@Param(value = "id_cat") Integer id_cat);
 }
