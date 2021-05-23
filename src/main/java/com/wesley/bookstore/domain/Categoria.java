389a1e72a849a6ee06c01c75b6f2fccc876e9263
wesleyrcastro <wesleyrcastro@yahoo.com.br>
2021-05-22 23:45:10 -0300

    ajustes finalizados no backend

--- src/main/java/com/wesley/bookstore/domain/Categoria.java
+++ src/main/java/com/wesley/bookstore/domain/Categoria.java
@@ -1,6 +1,9 @@
 package com.wesley.bookstore.domain;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +17,13 @@ public class Categoria implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotEmpty(message = "Campo NOME é requerido")
+    @Length(min = 3, max = 100, message = "O campo NOME deve ter entre 3 e 100 caracteres")
     private String nome;
+
+    @NotEmpty(message = "Campo DESCRIÇÂO é requerido")
+    @Length(min = 3, max = 200, message = "O campo DESCRIÇÂO deve ter entre 3 e 200 caracteres")
     private String descricao;
 
     @OneToMany(mappedBy = "categoria")
--- src/main/java/com/wesley/bookstore/dtos/CategoriaDTO.java
+++ src/main/java/com/wesley/bookstore/dtos/CategoriaDTO.java
@@ -1,7 +1,9 @@
 package com.wesley.bookstore.dtos;
 
 import com.wesley.bookstore.domain.Categoria;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 public class CategoriaDTO implements Serializable {
@@ -9,7 +11,12 @@ public class CategoriaDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer id;
+    @NotEmpty(message = "Campo NOME é requerido")
+    @Length(min = 3, max = 200, message = "O campo NOME deve ter entre 3 e 100 caracteres")
     private String nome;
+
+    @NotEmpty(message = "Campo DESCRIÇÂO é requerido")
+    @Length(min = 3, max = 200, message = "O campo DESCRIÇÂO deve ter entre 3 e 200 caracteres")
     private String descricao;
 
     public CategoriaDTO() {
