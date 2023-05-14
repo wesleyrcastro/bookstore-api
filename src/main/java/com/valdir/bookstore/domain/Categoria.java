3b88e410814dfe8569fbd748b18902f67dd21e68
wesleyrcastro <wesleyrcastro@yahoo.com.br>
2023-05-13 21:17:56 -0300

    Finalizando as classes de domínio

--- src/main/java/com/valdir/bookstore/domain/Categoria.java
+++ src/main/java/com/valdir/bookstore/domain/Categoria.java
@@ -2,17 +2,24 @@ package com.valdir.bookstore.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Entity
+@Entity
 @Data
-public class Categoria {
+public class Categoria implements Serializable {
 
+    private static final long serialVERSIONuid = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
     public Categoria() {
--- src/main/java/com/valdir/bookstore/domain/Livro.java
+++ src/main/java/com/valdir/bookstore/domain/Livro.java
@@ -2,14 +2,24 @@ package com.valdir.bookstore.domain;
 
 import lombok.Data;
 
-@Data
-public class Livro {
+import javax.persistence.*;
+import java.io.Serializable;
 
+@Entity
+@Data
+public class Livro implements Serializable {
+
+    private static final long serialVERSIONuid = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nomeAutor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
     public Livro() {
