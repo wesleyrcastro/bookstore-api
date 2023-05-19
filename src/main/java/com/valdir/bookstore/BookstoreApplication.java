1e4eae8132f6f1ca1822c03953bad7fc3af8d829
wesleyrcastro <wesleyrcastro@yahoo.com.br>
2023-05-19 15:57:00 -0300

    instancia de forma automatica da base de dados

--- src/main/java/com/valdir/bookstore/BookstoreApplication.java
+++ src/main/java/com/valdir/bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package com.valdir.bookstore;
 
+import com.valdir.bookstore.domain.Categoria;
+import com.valdir.bookstore.domain.Livro;
+import com.valdir.bookstore.repositories.CategoriaRepository;
+import com.valdir.bookstore.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "Livros de TI");
+
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+
+		cat1.getLivros().addAll(Arrays.asList(l1));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+	}
 }
