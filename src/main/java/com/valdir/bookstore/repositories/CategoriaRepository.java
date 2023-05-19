b56331382191981ecf42f422a2c76d2b981d085b
wesleyrcastro <wesleyrcastro@yahoo.com.br>
2023-05-19 15:00:39 -0300

    repositories implementando

---
+++ src/main/java/com/valdir/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.valdir.bookstore.repositories;
+
+import com.valdir.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/com/valdir/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,10 @@
+package com.valdir.bookstore.repositories;
+
+import com.valdir.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+}
